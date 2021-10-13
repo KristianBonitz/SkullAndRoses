8e8022f913f23f7631d3d575298fb8528aba2c85
Kristan Bonitz <kristian@bonitz.me>
2021-10-14 08:04:34 +1100

    Deleting all the .spec.ts files
    They weren't being used, so why have them at all?

--- AngularChatroom/ClientApp/src/app/message-types.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MessageTypes } from './message-types';
-
-describe('MessageTypes', () => {
-  it('should create an instance', () => {
-    expect(new MessageTypes()).toBeTruthy();
-  });
-});
